12350fddacb7d01890d12da9af0d5da2f8964789
Szymon Świędrych <szymek_sw@outlook.com>
2022-10-07 15:59:32 +0200

    Start Of the Project

---
+++ Lib/include/Vector3.h
@@ -0,0 +1,13 @@
+#ifndef S1NU5GRAPHICS_VECTOR3_H
+#define S1NU5GRAPHICS_VECTOR3_H
+
+namespace SG {
+
+    class Vector3 {
+    public:
+        static void Test();
+    };
+
+}
+
+#endif //S1NU5GRAPHICS_VECTOR3_H
---
+++ Main/main.cpp
@@ -0,0 +1,6 @@
+#include "Vector3.h"
+
+int main(int argc, char *argv[]) {
+    SG::Vector3::Test();
+    return 0;
+}
