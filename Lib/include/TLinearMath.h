b11993b055c1cab1208fb5c93f15adcacc02036e
Szymon Świędrych <szymek_sw@outlook.com>
2022-12-14 20:50:40 +0100

    Add plane line intersection

--- Lib/include/TLinearMath.h
+++ Lib/include/TLinearMath.h
@@ -8,14 +8,39 @@ namespace SG {
     struct TLine {
         TVector3<T> vector;
         TVector3<T> point;
+
+        static TLine<T> FromTwoPoints(TVector3<T> pointA, TVector3<T> pointB) {
+            TVector3 foundVector = pointB - pointA;
+            return {foundVector, pointA};
+        }
     };
+
+    template<typename T>
+    struct TPlane {
+        TVector3<T> normalVector;
+        TVector3<T> point;
+
+        static TPlane<T> FromGeneral(T a, T b, T c, T w) {
+            TVector3<T> foundNormalVector = {a, b, c};
+            TVector3<T> point;
+
+            if (c != 0) {
+                point = {0, 0, -w / c};
+            } else if (b != 0) {
+                point = {0, -w / b, c};
+            }
+
+            return TPlane{foundNormalVector, point};
+        }
+    };
+
 
     template<typename T>
     class TLinearMath {
     public:
 
-        static TVector3<T> FindIntersection(TLine<T> one, TLine<T> another) {
-            TVector3<T> deltaPoints = another.point - one.point;
+        static TVector3 <T> FindIntersection(TLine <T> one, TLine <T> another) {
+            TVector3 < T > deltaPoints = another.point - one.point;
 
             if (std::abs(deltaPoints.Dot(one.vector.Cross(another.vector))) > 0.001) {
                 throw std::exception();
@@ -29,13 +54,19 @@ namespace SG {
             return one.point + one.vector * s;
         }
 
-        static T FindAngle(TLine<T> one, TLine<T> another) {
+        static T FindAngle(TLine <T> one, TLine <T> another) {
             return one.vector.Normal().Dot(another.vector.Normal());
         }
 
-        static T Distance(TLine<T> one, TLine<T> another) {
+        static T FindLinesDistance(TLine <T> one, TLine <T> another) {
             return (another.point - one.point).Cross(another.vector).Dot(one.vector.Cross(another.vector)) /
                    one.vector.Cross(another.vector).Dot((one.vector.Cross(another.vector)));
+        }
+
+        static TVector3 <T> FindIntersection(TLine <T> line, TPlane<T> plane) {
+            T distance = plane.normalVector.Dot(plane.point) * -1.f;
+            T t = -(line.point.Dot(plane.normalVector) + distance) / line.vector.Dot(plane.normalVector);
+            return line.point + (plane.normalVector * t);
         }
     };
 }
--- Main/main.cpp
+++ Main/main.cpp
@@ -7,11 +7,17 @@
 #include <cmath>
 
 int main(int argc, char *argv[]) {
-    SG::TLine<double> lineOne{{3, 1, 5}, {2, -4, 0}};
-    SG::TLine<double> lineTwo{{1, -5, 3}, {2, -4, 0}};
+    SG::TLine<double> lineOne{{3, 1, 5}, {-2, 4, 0}};
+    SG::TLine<double> lineTwo{{1, -5, 3}, {-2, 4, 0}};
+    SG::TLine<double> lineThree{{3, -1, 2}, {-2, 2, -1}};
+
+    SG::TPlane<double> planeOne = SG::TPlane<double>::FromGeneral(2, 3, 3, -8);
+
+    std::cout << "Test: " << planeOne.normalVector.Normal() << std::endl;
 
     std::cout << "Zad 1.: " << SG::TLinearMath<double>::FindIntersection(lineOne, lineTwo) << std::endl;
     std::cout << "Zad 2.: " << SG::TLinearMath<double>::FindAngle(lineOne, lineTwo) << std::endl;
+    std::cout << "Zad 3.: " << SG::TLinearMath<double>::FindIntersection(lineThree, planeOne) << std::endl;
 
     return 0;
 }
