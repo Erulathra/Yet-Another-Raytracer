a4d4bf92a1ab661173b6d691d7a1d2cc45f667cf
Szymon Świędrych <szymek_sw@outlook.com>
2022-12-15 13:37:44 +0100

    Add another intersections (some needs tweaking)

--- Lib/include/TLinearMath.h
+++ Lib/include/TLinearMath.h
@@ -2,6 +2,7 @@
 
 #include "TVector3.h"
 #include <cmath>
+#include <ostream>
 
 namespace SG {
     class NoIntersectionException : std::exception {
@@ -9,12 +10,36 @@ namespace SG {
 
     template<typename T>
     struct TLine {
-        TVector3<T> vector;
-        TVector3<T> point;
+        TVector3 <T> vector;
+        TVector3 <T> point;
 
-        static TLine<T> FromTwoPoints(TVector3<T> pointA, TVector3<T> pointB) {
+    public:
+        static TLine<T> FromTwoPoints(TVector3 <T> pointA, TVector3 <T> pointB) {
             TVector3 foundVector = pointB - pointA;
             return {foundVector, pointA};
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const TLine& line) {
+            os << line.vector << " + " << line.point << "t";
+            return os;
+        }
+    };
+
+    template<typename T>
+    struct TSphere {
+        TVector3 <T> center;
+        T radius;
+    };
+
+    template<typename T>
+    struct TSegment {
+        TVector3 <T> pointOne;
+        TVector3 <T> pointTwo;
+        TLine <T> line;
+
+        TSegment(TVector3 <T> start, TVector3 <T> end)
+                : pointOne(start), pointTwo(end), line(TLine < T > ::FromTwoPoints(start, end)) {
+
         }
     };
 
@@ -42,8 +67,8 @@ namespace SG {
     class TLinearMath {
     public:
 
-        static TVector3<T> FindIntersection(TLine<T> one, TLine<T> another) {
-            TVector3<T> deltaPoints = another.point - one.point;
+        static TVector3 <T> FindIntersection(TLine<T> one, TLine<T> another) {
+            TVector3 < T > deltaPoints = another.point - one.point;
 
             if (std::abs(deltaPoints.Dot(one.vector.Cross(another.vector))) > 0.001) {
                 throw NoIntersectionException();
@@ -66,14 +91,14 @@ namespace SG {
                    one.vector.Cross(another.vector).Dot((one.vector.Cross(another.vector)));
         }
 
-        static TVector3<T> FindIntersection(TLine<T> line, TPlane<T> plane) {
+        static TVector3 <T> FindIntersection(TLine<T> line, TPlane <T> plane) {
             T distance = plane.normalVector.Dot(plane.point) * -1.f;
             T t = -(line.point.Dot(plane.normalVector) + distance) / line.vector.Dot(plane.normalVector);
             return line.point + (plane.normalVector * t);
         }
 
-        static TLine<T> FindIntersection(TPlane<T> planeOne, TPlane<T> planeTwo) {
-            TVector3<T> thirdPlaneNormal = planeOne.normalVector.Cross(planeTwo.normalVector);
+        static TLine<T> FindIntersection(TPlane <T> planeOne, TPlane <T> planeTwo) {
+            TVector3 < T > thirdPlaneNormal = planeOne.normalVector.Cross(planeTwo.normalVector);
             T det = thirdPlaneNormal.SquaredLength();
 
             if (det == 0.f)
@@ -86,8 +111,37 @@ namespace SG {
             return result;
         }
 
-        static T FindAngle(TLine<T> line, TPlane<T> plane) {
+        static T FindAngle(TLine<T> line, TPlane <T> plane) {
             return std::asin(line.vector.Normal().Dot(plane.normalVector.Normal()));
+        }
+
+        static T FindAngle(TPlane <T> planeOne, TPlane <T> planeTwo) {
+            return std::acos(planeOne.normalVector.Normal().Dot(planeTwo.normalVector.Normal()));
+        }
+
+        static TVector3 <T> FindIntersection(TSegment <T> SegmentOne, TSegment <T> SegmentTwo) {
+            TVector3 < T > result = FindIntersection(SegmentOne.line, SegmentTwo.line);
+            if ((SegmentOne.pointTwo - SegmentOne.pointOne).Dot(result - SegmentOne.pointOne) < 0 ||
+                (SegmentTwo.pointTwo - SegmentTwo.pointOne).Dot(result - SegmentTwo.pointOne) < 0) {
+                throw NoIntersectionException();
+            }
+
+            return result;
+        }
+
+        static std::vector<TVector3<T>> FindIntersection(TLine<T> line, TSphere<T> sphere) {
+            TVector3<T> centerToLineVector = line.point - sphere.center;
+            T a = line.vector.Dot(line.vector);
+            T b = 2. * centerToLineVector.Dot(line.vector);
+            T c = centerToLineVector.Dot(centerToLineVector) - sphere.radius * sphere.radius;
+            T delta = b*b - 4*a*c;
+            if (delta < 0.f)
+                throw NoIntersectionException();
+
+            T solutionOne = (-b - sqrt(delta) / 2. * a);
+            T solutionTwo = (-b + sqrt(delta) / 2. * a);
+
+            return {line.point + line.vector * solutionOne, line.point + line.vector * solutionTwo};
         }
     };
 }
--- Main/main.cpp
+++ Main/main.cpp
@@ -13,10 +13,25 @@ int main(int argc, char *argv[]) {
 
     SG::TPlane<double> planeOne = SG::TPlane<double>::FromGeneral(2, 3, 3, -8);
 
+    SG::TPlane<double> planeTwo = SG::TPlane<double>::FromGeneral(2, -1, 1, -8);
+    SG::TPlane<double> planeThree = SG::TPlane<double>::FromGeneral(4, 3, 1, 14);
+
     std::cout << "Zad 1.: " << SG::TLinearMath<double>::FindIntersection(lineOne, lineTwo) << std::endl;
     std::cout << "Zad 2.: " << SG::TLinearMath<double>::FindAngle(lineOne, lineTwo) << std::endl;
     std::cout << "Zad 3.: " << SG::TLinearMath<double>::FindIntersection(lineThree, planeOne) << std::endl;
-    std::cout << "Zad 3.: " << SG::TLinearMath<double>::FindAngle(lineThree, planeOne) << std::endl;
+    std::cout << "Zad 4.: " << SG::TLinearMath<double>::FindAngle(lineThree, planeOne) << std::endl;
+    SG::TLine result = SG::TLinearMath<double>::FindIntersection(planeTwo, planeThree);
+    std::cout << "Zad 5.: " << result << std::endl;
+    std::cout << "Zad 6.: " << SG::TLinearMath<double>::FindAngle(planeTwo, planeThree) << std::endl;
+
+    SG::TSegment<double> segmentOne({5, 5, 4}, {10, 10, 6});
+    SG::TSegment<double> segmentTwo({5, 5, 5}, {10, 10, 3});
+    std::cout << "Zad 7.: " << SG::TLinearMath<double>::FindIntersection(segmentOne, segmentTwo) << std::endl;
+
+    SG::TLine<double> lineX = SG::TLine<double>::FromTwoPoints({3, -1, -2}, {5, 3, -4});
+    SG::TSphere<double> sphere{{0, 0, 0}, std::sqrt(26)};
+    auto resultTwo = SG::TLinearMath<double>::FindIntersection(lineX, sphere);
+    std::cout << "Zad 8.: " << resultTwo[0] << " " << resultTwo[1] << std::endl;
 
     return 0;
 }
