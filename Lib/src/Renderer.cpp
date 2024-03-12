f6a53996255607c8a6d9f8cca1cc2ea4dd0cddab
Erulathra <43550433+Erulathra@users.noreply.github.com>
2024-03-12 14:34:10 +0100

    Add orthogonal camera

--- Lib/src/Renderer.cpp
+++ Lib/src/Renderer.cpp
@@ -1,4 +1,6 @@
 #include "Renderer.h"
+
+#include "Camera.h"
 #include "iostream"
 #include "LinearMath.h"
 
@@ -26,27 +28,12 @@ namespace YAR {
     }
 
     void Renderer::RayCast(const Vector3& cameraPosition, const Vector3& cameraDirection) {
-        const double pixelSize = 1. / 60.;
-        const double screenDistance = 1.f;
-
-        const Vector3 screenPosition = cameraPosition + cameraDirection * screenDistance;
-
-        Vector3 worldUp{0., 1., 0.};
-
-        if (cameraDirection.IsNear(Vector3{0, 1, 0}, 0.1)
-        || cameraDirection.IsNear(Vector3{0, 1, 0}, 0.1)) {
-            worldUp = Vector3(0., 0., 1.);
-        }
-
-        const Vector3 screenLeft = -(screenPosition - cameraPosition).Cross(worldUp).Normal();
-        const Vector3 screenDown = screenPosition.Cross(screenLeft).Normal();
+        OrthoCamera camera{cameraPosition, cameraDirection, 5.f, 5.f};
 
         for (int i = 0; i < 60; ++i) {
             for (int j = 0; j < 60; ++j) {
-                Vector3 pixelLocation = screenPosition + ((j - 30) * pixelSize) * screenLeft;
-                pixelLocation = pixelLocation + ((i - 30) * pixelSize) * screenDown;
+                Ray ray = camera.GetRay(i, j, 60, 60);
 
-                Ray ray = Ray::FromTwoPoints(cameraPosition, pixelLocation);
                 Sphere sphere {{0.f}, 1.f};
                 std::vector<Vector3> result;
                 pixels[i][j] = LinearMath::FindIntersection(ray, sphere, result);
--- Main/main.cpp
+++ Main/main.cpp
@@ -6,26 +6,9 @@
 int main(int argc, char* argv[]) {
     YAR::Renderer renderer;
 
-    double yaw = 30.;
-    double pitch = 30.;
-    double radius = 5.;
-
-    std::cout << "Pitch: " << pitch << " Yaw: " << yaw << " Radius: " << radius << "         " << std::endl;
-
-    if (pitch >= 360.)
-        pitch -= 360.;
-    else if (pitch < 0.)
-        pitch += 360.;
-
-    Vector3 cameraPosition(0.f);
-
-    const double pitchRadians = pitch * M_PI / 180.;
-    const double yawRadians = yaw * M_PI / 180.;
-    cameraPosition.x = radius * std::cos(pitchRadians) * std::cos(yawRadians);
-    cameraPosition.y = radius * std::sin(pitchRadians) * std::cos(yawRadians);
-    cameraPosition.z = radius * std::sin(yawRadians);
-
-    Vector3 cameraDirection = (-cameraPosition).Normal();
+    Vector3 cameraPosition(0.f, 0.f, -5.f);
+    Vector3 cameraDirection (0.f, 0.f, 1.f);
+    cameraDirection = cameraDirection.Normal();
 
     renderer.RayCast(cameraPosition, cameraDirection);
     renderer.Draw();
